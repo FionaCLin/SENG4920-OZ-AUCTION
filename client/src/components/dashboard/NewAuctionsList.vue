5dbbfe0f4b06d208e5b3ee96448b487b10692116
FionaCLin <fiona.lin@student.unsw.edu.au>
2019-10-12 17:32:24 +1100

    Make dashboard components

---
+++ client/src/components/dashboard/NewAuctionsList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="q-pa-md">
+    <h5>
+      <a name="newAuctions">New Auctions</a>
+    </h5>
+
+    <div
+      class="fit row wrap justify-start items-start content-start  q-gutter-sm"
+    >
+      <NewAuctionItem v-for="n in 5" :key="n" />
+    </div>
+  </div>
+</template>
+<script>
+import NewAuctionItem from "./NewItem";
+export default {
+  name: "NewAuction",
+  components: {
+    NewAuctionItem
+  },
+  data() {
+    return {
+      slide: 1
+    };
+  }
+};
+</script>
+<style lang="sass" scoped>
+.custom-caption
+  text-align: center
+  padding: 12px
+  color: white
+  background-color: rgba(0, 0, 0, .3)
+</style>
