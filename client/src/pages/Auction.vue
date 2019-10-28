ca01f7d3a919c03ddb3ff8515f47235bf124de2d
FionaCLin <fiona.lin@student.unsw.edu.au>
2019-10-28 13:37:31 +0800

    Add mock up auction item detail page

---
+++ client/src/pages/Auction.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page padding>
+    <q-toolbar>
+      <q-btn to="/create"> <q-icon name="add" />Create Auction </q-btn>
+    </q-toolbar>
+    {{ auction }}
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "AuctionPage",
+  data() {
+    return {
+      auction: this.$store.state.auction.myAuctions.auction_items.find(
+        x => x.id === this.$route.params.id
+      )
+    };
+  },
+  created() {
+    console.log("to", this.$route.params.id);
+  }
+};
+</script>
